aaa8f65e347778d79193e1215fe30581a15f1e6f
Venkat Ram Prakash Kotni <vkotni@codeaurora.org>
2013-11-11 14:53:06 -0800

    Refactor IMSI and SubscriberId handling for CDMAPhone/CDMALTEPhone
    IccRecords
    CdmaServiceStateTracker does not need to set IMSI on an IccRecord.
    CDMAPhone
    When queried for the subscriber Id return the IMSI from
    CdmaServiceStateTracker or the card depending on whether the active
    subscription source is NV or RUIM respectively.
    
    Change-Id: Id613f2df91b94ae5d7613dfcef3e097cb817ff4e
    CRs-Fixed: 323977,323977,342146,505580
    
    Conflicts:
    	src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java

--- src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
+++ src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
@@ -407,17 +407,6 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
                     mIsMinInfoReady = true;
 
                     updateOtaspState();
-                    if (!mIsSubscriptionFromRuim && mIccRecords != null) {
-                        if (DBG) {
-                            log("GET_CDMA_SUBSCRIPTION set imsi in mIccRecords");
-                        }
-                        mIccRecords.setImsi(getImsi());
-                    } else {
-                        if (DBG) {
-                            log("GET_CDMA_SUBSCRIPTION either mIccRecords is null  or NV type device" +
-                                    " - not setting Imsi in mIccRecords");
-                        }
-                    }
                 } else {
                     if (DBG) {
                         log("GET_CDMA_SUBSCRIPTION: error parsing cdmaSubscription params num="
@@ -1624,12 +1613,11 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
     }
 
     /**
-     * Returns IMSI as MCC + MNC + MIN
+     * Returns IMSI from NV in the format MCC + MNC + MIN
      */
-    String getImsi() {
-        // TODO: When RUIM is enabled, IMSI will come from RUIM not build-time props.
-        String operatorNumeric = SystemProperties.get(
-                TelephonyProperties.PROPERTY_ICC_OPERATOR_NUMERIC, "");
+    public String getNvImsi() {
+        String operatorNumeric = getSystemProperty(
+                CDMAPhone.PROPERTY_CDMA_HOME_OPERATOR_NUMERIC, "");
 
         if (!TextUtils.isEmpty(operatorNumeric) && getCdmaMin() != null) {
             return (operatorNumeric + getCdmaMin());
--- src/java/com/android/internal/telephony/uicc/RuimRecords.java
+++ src/java/com/android/internal/telephony/uicc/RuimRecords.java
@@ -151,11 +151,6 @@ public final class RuimRecords extends IccRecords {
         mRecordsRequested = false;
     }
 
-    @Override
-    public String getIMSI() {
-        return mImsi;
-    }
-
     public String getMdnNumber() {
         return mMyMobileNumber;
     }
